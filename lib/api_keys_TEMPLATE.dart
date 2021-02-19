1c6655f49d9ae37efbb4fbf29f37b811661ac481
Robert Marzec <robertmarzecj@gmail.com>
2021-02-18 19:26:17 -0500

    updated the instructions for using a custom Google Places API key to the api_keys_TEMPLATE.dart file

--- lib/api_keys_TEMPLATE.dart
+++ lib/api_keys_TEMPLATE.dart
@@ -1,3 +1,6 @@
 class ApiKeys {
+  // if you would like to mess around with this project, rename this file to
+  // "api_keys.dart" and fill in your Google Places API key below for full
+  // working functionality
   static String googlePlacesKey = '[YOUR GOOGLE PLACES API KEY HERE]';
 }
