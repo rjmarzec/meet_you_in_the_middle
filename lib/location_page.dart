8fea6d2f95c7d096ee8bebbabcc89f958bfc1bd4
Robert Marzec <robertmarzecj@gmail.com>
2021-02-18 19:25:44 -0500

    removed a now unused package import that was throwing an error

--- lib/location_page.dart
+++ lib/location_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'location_manager.dart';
 
 class LocationPage extends StatefulWidget {
