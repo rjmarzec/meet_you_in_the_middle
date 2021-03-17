1a55b64782891d472f14cb6988d85dc09e193892
Robert Marzec <robertmarzecj@gmail.com>
2021-03-17 03:31:29 -0400

    filled in the app information page

--- lib/app_info_dialog.dart
+++ lib/app_info_dialog.dart
@@ -15,15 +15,18 @@ class AppInfoDialogState extends State<AppInfoDialog> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text("[APP INFO HERE]"),
+            Text(
+              "Finally, a fair meetup coordination tool!\n\nHit the + button to get started by adding a location from a search, a saved favorite, or wherever you're currently standing.\n\nOnce you're done, switch to the map screen and tap the midpoint to open it up in Google Maps.\n\nYou've got your fair midpoint, and from there it's up to you to decide what spot looks best.\n\n---------------------------------------------\n\nDeveloped by Robert Marzec using Flutter. Find out more at rjmarzec.com",
+              textAlign: TextAlign.center,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                FlatButton(
+                TextButton(
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Text("Done"),
+                  child: Text("DONE"),
                 ),
               ],
             )
